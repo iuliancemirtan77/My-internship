5d726aa0ae7786dc4ce42a5850ba40f632f3e6b3
iulian.cemirtan <iulian.cemirtan@MDDSK40049>
2016-06-22 15:58:06 +0300

    all file added

---
+++ InternGit/InternGit/Boots.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InternGit
+{
+    public class Boots
+    {
+        private int function;
+    }
+}
---
+++ InternGit/InternGit/Shoes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InternGit
+{
+    internal class Shoes
+    {
+        private int size;
+        private int colour;
+        private int technology;
+    }
+}
